f4d77d97e16d0b01064e8b35865006d4227cbb90
nenchin <nenchin2001@gmail.com>
2020-02-25 13:56:42 +0200

    first commit

--- src/Main/MainWindow.java
+++ src/Main/MainWindow.java
@@ -1,4 +1,28 @@
 package Main;
 
+import Clone.DollySheet;
+
 public class MainWindow {
+    public static void Main(String[] args) {
+        DollySheet dollySheet = new DollySheet();
+        DollySheet dollySheet2= foo(dollySheet);
+
+        dollySheet.setName("Dolly");
+        dollySheet2.setName("Dolly2Sheet");
+
+        System.out.println(dollySheet.getName());
+        System.out.println(dollySheet2.getName());
+
+    }
+    public static DollySheet foo(DollySheet dollySheet2){
+        DollySheet sheet = null;
+        try {
+            sheet = (DollySheet)dollySheet2.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return sheet;
+    }
+
+
 }
